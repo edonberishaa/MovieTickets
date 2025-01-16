20fbb2be960f8d3ce1ee3bb885befba60dc028af
Edon Berisha <edonberisha52@gmail.com>
2025-01-16 01:10:08 +0100

    h

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -18,20 +18,28 @@ namespace MovieTickets.Controllers
             var data = await _service.GetAllAsync();
             return View(data);
         }
-        //Get: Actors/Create
-        public IActionResult Create()
+        //Get: Actors/Edit/1
+        public async Task<IActionResult> Edit(int id)
         {
-            return View();
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(actorDetails);
+            }
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Actor actor)
+        public async Task<IActionResult> Edit(int id,[Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
         {
             if (!ModelState.IsValid)
             {
                 return View(actor);
             }
-            await _service.AddAsync(actor);
+            await _service.UpdateAsync(id,actor);
             return RedirectToAction(nameof(Index));
         }
         public async Task<IActionResult> Details(int id)
@@ -46,6 +54,37 @@ namespace MovieTickets.Controllers
                 return View(actorDetails);
             }
         }
+
+        //Get: Actors/Delete/1
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(actorDetails);
+            }
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                await _service.DeleteAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
 
     }
 }
